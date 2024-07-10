62256379713c83eebfd082f9e07553f323aa4a8f
Kshitij <kshitijkota22@gmail.com>
2024-07-10 10:50:33 +0530

    Added usage of .env file in app.py

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
-import sys
 import os
+from dotenv import load_dotenv
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import cv2
@@ -7,8 +7,11 @@ import numpy as np
 import tensorflow as tf
 import requests
 
+# Load environment variables from .env file
+load_dotenv()
+
 # Add the root directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from hash import histogram_hash
 
 app = Flask(__name__)
@@ -18,15 +21,14 @@ app.config["UPLOAD_FOLDER"] = os.path.join(os.getcwd(), "static/uploads/")
 os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 # Google Drive file ID and download URL
-file_id = "1875CZkQWUZDlehjCWxURdTSa6n2_xf0s"
-model_url = f"https://drive.google.com/uc?export=download&id={file_id}"
+model_url = os.getenv("MODEL_URL")
 model_path = os.path.join(os.getcwd(), "api/saved_model/my_model.keras")
 
 # Debug: Check the model path
 print(f"Model path: {model_path}")
 
 # Download the model from Google Drive if it does not exist locally
-if not os.path.exists(model_path):
+if not os.path.exists(model_path) or os.path.getsize(model_path) == 0:
     print("Model file does not exist locally. Downloading...")
     os.makedirs(os.path.dirname(model_path), exist_ok=True)
     response = requests.get(model_url)
