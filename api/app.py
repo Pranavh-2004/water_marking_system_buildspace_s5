6b4fe20777cde48f2fe5ef4d534daf0cf8960e7f
Kshitij <kshitijkota22@gmail.com>
2024-07-10 13:18:51 +0530

    chore: Update app.py to use TensorFlow instead of Keras

--- api/app.py
+++ api/app.py
@@ -5,8 +5,9 @@ from werkzeug.utils import secure_filename
 import cv2
 import numpy as np
 import tensorflow as tf
+import keras
 import requests
-from tensorflow import keras
+import gdown
 
 # Add the root directory to the Python path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -28,37 +29,8 @@ print(f"Model path: {model_path}")
 
 
 def download_file_from_google_drive(file_id, destination):
-    # Define the base URL for downloading from Google Drive
-    URL = "https://docs.google.com/uc?export=download"
-
-    # Initiate the session
-    session = requests.Session()
-
-    # Define the response
-    response = session.get(URL, params={"id": file_id}, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {"id": file_id, "confirm": token}
-        response = session.get(URL, params=params, stream=True)
-
-    save_response_content(response, destination)
-
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith("download_warning"):
-            return value
-    return None
-
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    gdown.download(url, destination, quiet=False)
 
 
 # Download the model from Google Drive if it does not exist locally
